4c2485bf6f0d1e99053b635cb5a7befa4bfaccff
Eugene Stashkevych <eugene.kevich@gmail.com>
2022-09-08 16:49:12 +0200

    update filesi

---
+++ index.js
@@ -0,0 +1,75 @@
+// import packages
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import pretty from 'pretty';
+
+const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+const urlImg = [];
+
+// scrap data
+async function scrapeData() {
+  try {
+    // Fetch HTML of the page we want to scrape
+    const { data } = await axios.get(url);
+    // console.log(data);
+
+    // Load HTML we fetched in the previous line
+    const $ = cheerio.load(data);
+    // console.log($);
+
+    // extract all img-links with an attribute src
+    $('img', data).each(function () {
+      const img = $(this).attr('src');
+
+      // add all links to an array
+      urlImg.push(img);
+    });
+    // console.log(urlImg);
+
+    // new array with 10 img
+    const firstTenImg = urlImg.slice(0, 10);
+    console.log(firstTenImg);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+scrapeData();
+
+// scrapeData();
+
+/* async function scrapeData() {
+  try {
+    // Fetch HTML of the page we want to scrape
+    const { data } = await axios.get(url);
+    // Load HTML we fetched in the previous line
+    const $ = cheerio.load(data);
+    // Select all the list items in plainlist class
+    const listItems = $(".plainlist ul li");
+    // Stores data for all countries
+    const countries = [];
+    // Use .each method to loop through the li we selected
+    listItems.each((idx, el) => {
+      // Object holding data for each country/jurisdiction
+      const country = { name: "", iso3: "" };
+      // Select the text content of a and span elements
+      // Store the textcontent in the above object
+      country.name = $(el).children("a").text();
+      country.iso3 = $(el).children("span").text();
+      // Populate countries array with country data
+      countries.push(country);
+    });
+    // Logs countries array to the console
+    console.dir(countries);
+    // Write countries array in countries.json file
+    fs.writeFile("coutries.json", JSON.stringify(countries, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Successfully written data to file");
+    });
+  } catch (err) {
+    console.error(err);
+  }
+} */
